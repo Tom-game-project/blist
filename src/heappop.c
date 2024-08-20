276d04e29cc9ebcf529d9ebe68c062ff6a709110
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-08-20 13:20:49 +0900

    [WIP] add heapq data file

---
+++ src/heappop.c
@@ -0,0 +1,7 @@
+#include "blist.h"
+
+/// @brief  ヒープキューに見立てた配列として、最初の要素からデータを取り出し返します
+/// @return 
+t_node *heappop(){
+    
+}
---
+++ src/heappush.c
@@ -0,0 +1,8 @@
+#include <blist.h>
+
+// /// @brief  実装する予定
+// void heappush(t_node **lst, t_node *node) {
+//     int current_index = 
+
+//     push(lst, node);
+// }
